e9baf72ec541386dc6811c56244aa79a3eb8a80e
Ярослав <aroslavprocuk607@gmail.com>
2024-09-09 20:39:39 +0300

    Add files via upload

---
+++ Практична 1 (v1).py
@@ -0,0 +1,17 @@
+Changeable1 = 'Hello World'
+print (" ім'я: ")
+Changeable2 = input()
+print (' Прізвище: ')
+Changeable3 = input()
+print (' Вік: ')
+Changeable4 = input()
+
+print (Changeable1)
+print (Changeable2)
+print (Changeable3)
+print (Changeable4)
+
+print (type(Changeable1))
+print (type(Changeable2))
+print (type(Changeable3))
+print (type(Changeable4))
